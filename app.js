35aa6ccab6f917b1deab2e86b5f0ec3a52d49678
JOAQUINESTEVEZ <joaquin2001estevez@gmail.com>
2024-05-30 17:26:02 -0300

    sign up added

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-const express = require('express');
-const handlers = require('./lib/handlers');
-let app = express();
+const express = require('express')
+const weatherMiddlware = require('./lib/middleware/weather')
+const handlers = require('./lib/handlers')
+const bodyParser = require('body-parser')
 
-// Set up handlebars view engine
-let handlebars = require('express-handlebars')
-	.create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3000);
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// Directories
+// Configure Handlebars view engine
+let handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
+})
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddlware)
 
 // Routes
 app.get('/', handlers.home)
-app.get('/about', handlers.about)
-app.use(handlers.notFound) //custom 404 page
-app.use(handlers.serverError) //custom 500 page
+app.get('/section-test', handlers.sectionTest)
+app.get('/newsletter-signup', handlers.newsletterSignUp)
+app.post('/newsletter-signup/process', handlers.newsletterSignUpProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 
-// Port
-if(require.main === module){
-    app.listen(port, () => {
-        console.log(`Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.')
-    })
-} else{
-    module.exports = app
+app.use(handlers.notFound)
+app.use(handlers.serverError)
+
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` +
+      '; press Ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
 }
