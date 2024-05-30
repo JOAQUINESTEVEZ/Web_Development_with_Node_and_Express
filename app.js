dae068e2df6bfe1e6e841ae8bd157315d98b26f9
JOAQUINESTEVEZ <joaquin2001estevez@gmail.com>
2024-05-30 09:51:52 -0300

    unit and integration tests added

--- app.js
+++ app.js
@@ -1,44 +1,28 @@
 const express = require('express');
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers');
 let app = express();
 
-// set up handlebars view engine
+// Set up handlebars view engine
 let handlebars = require('express-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
 
+// Directories
 app.use(express.static(__dirname + '/public'))
 
+// Routes
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
+app.use(handlers.notFound) //custom 404 page
+app.use(handlers.serverError) //custom 500 page
 
-
-//routes
-app.get('/', (req,res) => {
-    res.render('home')
-})
-
-app.get('/about', (req,res) => {
-    res.render('about', { fortune: fortune.getFortune()})
-})
-
-//custom 404 page
-app.use((req,res) => {
-    res.status(404)
-    res.render('404')
-})
-
-//custom 500 page
-app.use((err,req,res,next) => {
-    console.log(err.message)
-    res.status(500)
-    res.render('500')
-})
-
-
-
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-      app.get('port') + '; press Ctrl-C to terminate.' );
-  });
+// Port
+if(require.main === module){
+    app.listen(port, () => {
+        console.log(`Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.')
+    })
+} else{
+    module.exports = app
+}
